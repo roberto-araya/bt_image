55a5204770a99aeb71420d267a402fd95e08f574
Roberto Araya <roberto.araya@btester.cl>
2018-01-18 10:52:51 -0300

    Issue #2937786 by roberto_araya: Clean code and apply coding standard

--- src/Config/ConfigImageOverride.php
+++ src/Config/ConfigImageOverride.php
@@ -1,7 +1,4 @@
 <?php
-/**
- *
- */
 
 namespace Drupal\bt_image\Config;
 
@@ -13,55 +10,67 @@ use Drupal\Core\Config\ConfigFactoryOverrideInterface;
  */
 class ConfigImageOverride implements ConfigFactoryOverrideInterface {
 
-    public function loadOverrides($names) {
-        $overrides = array();
+  /**
+   * {@inheritdoc}
+   */
+  public function loadOverrides($names) {
+    $overrides = array();
 
-        if (in_array('media_entity.bundle.image', $names)) {
-            //$overrides['media_entity.bundle.image']['label'] = 'Imagen';
-            //$overrides['media_entity.bundle.image']['description'] = 'Almacene una imagen localmente en su biblioteca multimedia.';
-        }
-        if (in_array('field.field.media.image.image', $names)) {
-            //$overrides['field.field.media.image.image']['label'] = 'Imagen';
-            //$overrides['field.field.media.image.image']['description'] = 'Suba una imagen a su catálogo de imágenes de la aplicación.';
-            $overrides['field.field.media.image.image']['settings']['file_directory'] = 'media_imagen';
-            $overrides['field.field.media.image.image']['settings']['alt_field'] = TRUE;
-            $overrides['field.field.media.image.image']['settings']['alt_field_required'] = FALSE;
-            $overrides['field.field.media.image.image']['settings']['title_field'] = TRUE;
-            $overrides['field.field.media.image.image']['settings']['title_field_required'] = FALSE;
-        }
-        // Workbench moderations settings
-        $third_party_settings = [
-            'workbench_moderation' => [
-                'enabled' => TRUE,
-                'allowed_moderation_states' => ['published', 'archived', 'draft', 'needs_review'],
-                'default_moderation_state' => 'draft'
-            ]
-        ];
-        // Add workbench moderation to image bundle
-        if (in_array('media_entity.bundle.image', $names)) {
-            $overrides['media_entity.bundle.image']['third_party_settings'] = $third_party_settings;
-        }
-        return $overrides;
+    if (in_array('media_entity.bundle.image', $names)) {
+      //$overrides['media_entity.bundle.image']['label'] = 'Imagen';
+      //$overrides['media_entity.bundle.image']['description'] = 'Almacene una imagen localmente en su biblioteca multimedia.';
+    }
+    if (in_array('field.field.media.image.image', $names)) {
+      //$overrides['field.field.media.image.image']['label'] = 'Imagen';
+      //$overrides['field.field.media.image.image']['description'] = 'Suba una imagen a su catálogo de imágenes de la aplicación.';
+      $overrides['field.field.media.image.image']['settings']['file_directory'] = 'media_imagen';
+      $overrides['field.field.media.image.image']['settings']['alt_field'] = TRUE;
+      $overrides['field.field.media.image.image']['settings']['alt_field_required'] = FALSE;
+      $overrides['field.field.media.image.image']['settings']['title_field'] = TRUE;
+      $overrides['field.field.media.image.image']['settings']['title_field_required'] = FALSE;
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getCacheSuffix() {
-        return 'ConfigImageOverride';
+    // Workbench moderations settings.
+    $third_party_settings = [
+      'workbench_moderation' => [
+        'enabled' => TRUE,
+        'allowed_moderation_states' => [
+          'published',
+          'archived',
+          'draft',
+          'needs_review',
+        ],
+        'default_moderation_state' => 'draft',
+      ],
+    ];
+
+    // Add workbench moderation to image bundle.
+    if (in_array('media_entity.bundle.image', $names)) {
+      $overrides['media_entity.bundle.image']['third_party_settings'] = $third_party_settings;
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getCacheableMetadata($name) {
-        return new CacheableMetadata();
-    }
+    return $overrides;
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function createConfigObject($name, $collection = StorageInterface::DEFAULT_COLLECTION) {
-        return NULL;
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheSuffix() {
+    return 'ConfigImageOverride';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheableMetadata($name) {
+    return new CacheableMetadata();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function createConfigObject($name, $collection = StorageInterface::DEFAULT_COLLECTION) {
+    return NULL;
+  }
+
 }
